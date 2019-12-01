4bcb266ae0a82570c2133044da5fa967601663af
Johan Hillerström <progr@mmer.nu>
2019-12-01 07:04:08 +0100

    Add day 1 in V

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2019
+
+## [V](https://vlang.io)
+
+```bash
+git clone https://github.com/hillerstorm/advent-of-code-2019
+# must be in 'v' folder for relative paths to work
+cd advent-of-code-2019/v
+v run day_<num>.v
+```
---
+++ v/day_01.v
@@ -0,0 +1,29 @@
+import (
+	os
+	math
+)
+
+fn main() {
+	input := os.read_lines('../inputs/20191201.txt') or {
+		println('input not found')
+		return
+	}
+	mut part1 := 0
+	mut part2 := 0
+	for line in input {
+		p1, p2 := fuel_req(line.int())
+		part1 += p1
+		part2 += p2
+	}
+	println('part 1: $part1')
+	println('part 2: $part2')
+}
+
+fn fuel_req(mass int) (int, int) {
+	req := int(math.floor(mass / 3) - 2)
+	if req <= 0 {
+		return req, 0
+	}
+	_, rest := fuel_req(req)
+	return req, req + rest
+}
