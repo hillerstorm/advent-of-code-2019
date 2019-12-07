e9ea064cc20b5deebd9ff4e1fa903853177016bf
Johan Hillerström <progr@mmer.nu>
2019-12-07 17:05:22 +0100

    Add day 6 in V

---
+++ v/day_06.v
@@ -0,0 +1,65 @@
+import (
+	os
+)
+
+fn main() {
+	input := os.read_lines('../inputs/20191206.txt')?
+
+	mut orbits := {
+		'COM': ''
+	}
+
+	for line in input {
+		split := line.trim_space().split(')')
+		orbits[split[1]] = split[0]
+	}
+
+	sum_direct := orbits.size - 1
+	mut sum_indirect := 0
+	mut san := []string
+	mut you := []string
+
+	for key, orb in orbits {
+		if orb == '' || orb == 'COM' {
+			continue
+		}
+
+		is_you := key == 'YOU'
+		is_san := key == 'SAN'
+		if is_you {
+			you << orb
+		} else if is_san {
+			san << orb
+		}
+
+		mut curr := orbits[orb]
+		mut indirect := 0
+		for curr != '' {
+			if is_you {
+				you << curr
+			} else if is_san {
+				san << curr
+			}
+
+			indirect++
+			curr = orbits[curr]
+		}
+
+		sum_indirect += indirect
+	}
+
+	println('part 1: ${sum_direct + sum_indirect}')
+
+	san = san.reverse()
+	you = you.reverse()
+
+	mut idx := 0
+	for i, s in san {
+			if s != you[i] {
+					idx = i
+					break
+			}
+	}
+
+	println('part 2: ${san[idx..].len + you[idx..].len}')
+}
