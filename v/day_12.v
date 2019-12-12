4f7aa9f46c972d0b9ad34d4992005bfc83a0d007
Johan Hillerström <progr@mmer.nu>
2019-12-12 19:50:37 +0100

    Add day 12 in V

---
+++ v/day_12.v
@@ -0,0 +1,117 @@
+import (
+	os
+	math
+)
+
+struct Moon {
+mut:
+	x int
+	y int
+	z int
+	vel_x int = 0
+	vel_y int = 0
+	vel_z int = 0
+}
+
+fn main() {
+	input := os.read_lines('../inputs/20191212.txt')?
+	moons := input.map(parse_moon(it))
+	println('part 1: ${moons.clone().part_one()}')
+	println('part 2: ${moons.part_two()}')
+}
+
+fn (moons []Moon) part_one() int {
+	for i := 0; i < 1000; i++ {
+		moons.step()
+	}
+	return moons.calculate_energy()
+}
+
+fn (moons []Moon) part_two() i64 {
+	mut lcm := i64(1)
+	for i := 0; i < 3; i++ {
+		val := cycles(moons.clone(), i)
+		lcm = math.lcm(lcm, val)
+	}
+	return lcm
+}
+
+fn (moons []Moon) step() {
+	for j := 0; j < moons.len; j++ {
+		mut moon := &moons[j]
+		for k := 0; k < moons.len; k++ {
+			if j == k {
+				continue
+			}
+
+			moon.modify_velocity(&moons[k])
+		}
+	}
+
+	for j := 0; j < moons.len; j++ {
+		mut moon := &moons[j]
+		moon.apply_velocity()
+	}
+}
+
+fn cycles(moons []Moon, idx int) i64 {
+	mut res := i64(1)
+	moons.step()
+	for !moons.velocity_zero(idx) {
+		moons.step()
+		res++
+	}
+
+	return res * 2
+}
+
+fn (moons []Moon) velocity_zero(idx int) bool {
+	zeros := moons.filter((idx == 0 && it.vel_x == 0) || (idx == 1 && it.vel_y == 0) || (idx == 2 && it.vel_z == 0))
+	return zeros.len == moons.len
+}
+
+fn (m mut Moon) modify_velocity(other &Moon) {
+	if other.x > m.x {
+		m.vel_x++
+	} else if other.x < m.x {
+		m.vel_x--
+	}
+
+	if other.y > m.y {
+		m.vel_y++
+	} else if other.y < m.y {
+		m.vel_y--
+	}
+
+	if other.z > m.z {
+		m.vel_z++
+	} else if other.z < m.z {
+		m.vel_z--
+	}
+}
+
+fn (m mut Moon) apply_velocity() {
+	m.x += m.vel_x
+	m.y += m.vel_y
+	m.z += m.vel_z
+}
+
+fn (m []Moon) calculate_energy() int {
+	mut sum := 0
+	for moon in m {
+		pot_energy := math.abs(moon.x) + math.abs(moon.y) + math.abs(moon.z)
+		kin_energy := math.abs(moon.vel_x) + math.abs(moon.vel_y) + math.abs(moon.vel_z)
+		sum += int(pot_energy * kin_energy)
+	}
+	return sum
+}
+
+fn parse_moon(line string) Moon {
+	parts := line.trim_space().replace('<x=', '').replace(' y=', '').replace(' z=', '').replace('>', '').split(',')
+	nums := parts.map(it.int())
+	return &Moon {
+		x: nums[0]
+		y: nums[1]
+		z: nums[2]
+	}
+}
